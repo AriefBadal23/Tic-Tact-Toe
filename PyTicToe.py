b06c4280fa27d24e17bec47ab2d1ebd7b4d187f8
Arief Badal <1042170@hr.nl>
2022-10-13 22:44:27 +0200

    Add ability to draw circle when its player 1 has his turn

--- PyTicToe.py
+++ PyTicToe.py
@@ -10,34 +10,37 @@ class Game():
 
 
     def run(self):
-        window = Window(600,600, (0, 153, 153))
+        window = Window(600,600)
         main_window = window.get_window()
         board = Board()
+        board.draw_lines(main_window)
 
         while self.running:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # The window has an height and width of 600 px; each square to mark is 200 px
+                    mouse_y = int(event.pos[0] // 200)
+                    mouse_x = int(event.pos[1] // 200)
+                    # print(mouse_x, mouse_y)
+                    board.check_available(mouse_x, mouse_y)
+                    if self.player == 1:
+                        board.draw_shape(mouse_x , mouse_y, self.player)
+                        pos = pygame.mouse.get_pos()
+                        x, y = pos
+                        # TODO: circle draws but disappears
+                        if pygame.mouse.get_pressed()[0]:
+                            board.draw_circle(main_window, x, y)
+                            pygame.display.update()   
+                            self.player = 2
+                        
+                    elif self.player == 2:
+                        board.draw_shape(mouse_x, mouse_y, self.player)
+                        self.player = 1
+
+            if event.type == pygame.QUIT:
                     self.running = False
                     pygame.quit()
-
-                if self.running:
-                    board.draw_lines(main_window)
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        # TODO: self.board blijft op 0,0,0 ??? hoe te fixen?
-                        # Bepaalde square geeft False terug en klik verander niet naar --> 2
-                        # The window has an height and width of 600 px; each square to mark is 200 px
-                        mouse_y = int(event.pos[0] // 200)
-                        mouse_x = int(event.pos[1] // 200)
-                        # TODO: player does not change if one player has already marked a spot
-
-                        if board.check_available(mouse_x, mouse_y):
-                            if self.player == 1:
-                                board.draw_shape(mouse_x, mouse_y, self.player)
-                                self.player = 2
-                            elif self.player == 2:
-                                board.draw_shape(mouse_x, mouse_y, self.player)
-                                self.player = 1
-                                
+                        
 
 
 
