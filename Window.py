eb030df33e216ab81ecc9111894a3561089012a4
Arief Badal <1042170@hr.nl>
2022-10-13 22:44:56 +0200

    Add background color attribute/instance variable

--- Window.py
+++ Window.py
@@ -1,15 +1,16 @@
 import pygame
 
 class Window():
-    def __init__(self, width, height, color):
+    def __init__(self, width, height):
         self.widht = width
         self.height = height
         self.running = True
-        self.color = color
+        self.color = (0, 153, 153)
 
 
     def get_window(self):
         window = pygame.display.set_mode((self.widht, self.height))
+        window.fill(self.color)
         return window
 
 
