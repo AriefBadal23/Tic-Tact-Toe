6728d3481ba3975d1b1e547f954a087c2a8aa9af
Arief Badal <1042170@hr.nl>
2022-11-13 19:43:04 +0100

    Refactor conform Pep8 convention

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,11 +3,14 @@ It is required to install pygame to run the game.
 it contains the follwoing methods:
     * run() - runs the game
 """
-import pygame
-from board import Board
-from Window import Window
-from game_over_screen import GameOverScreen
 import time
+
+import pygame
+
+from board import Board
+from game_over_screen import GameOverScreen
+from window import Window
+
 
 class Game:
     """A class to create tic-tac-toe game
@@ -48,29 +51,27 @@ class Game:
                     mouse_y = int(event.pos[0] // 200)
                     mouse_x = int(event.pos[1] // 200)
 
-
                     if board.check_available_square(mouse_x, mouse_y) is True:
                         if self.player == 1:
                             board.draw_shape(mouse_x, mouse_y, self.player)
                             if board.mark_board_full(mouse_x, mouse_y):
-                                    self.running = False
+                                self.running = False
                             mouse_position = pygame.mouse.get_pos()
                             x_pos, y_pos = mouse_position
                             if board.draw_circle(main_window, (0, 255, 0), x_pos, y_pos):
                                 if board.check_win(self.player, main_window) is True:
                                     pygame.display.update()
-                                    # TODO: knoppen met een Restart knop of quit knop
-                                    game_over_screen.draw_text(self.player, main_window, 200 , 300)
+                                    game_over_screen.draw_text(self.player,
+                                                               main_window, 150 , 300)
                                     time.sleep(5)
                                     self.running = False
-
-
                             self.player = 2
 
                         elif self.player == 2:
-                            """ mouse_x and mouse_y = uses the 3x3 raster
-                                and x_pos and y_post uses the real mouse position
-                             """
+                            # mouse_x and mouse_y = uses the 3x3 raster
+                            # and x_pos and y_post uses
+                            # the real mouse position
+                             
                             mouse_position = pygame.mouse.get_pos()
                             x_pos, y_pos = mouse_position
                             board.draw_shape(mouse_x, mouse_y, self.player)
@@ -78,6 +79,6 @@ class Game:
                             if board.check_win(self.player, main_window) is True:
                                 pygame.display.update()
                                 game_over_screen.draw_text(self.player, main_window, 200 , 300)
-                                time.sleep(5)
+                                time.sleep(5)                        
                                 self.running = False
-                            self.player = 1
+                            self.player = 1                            
