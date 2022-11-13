1ed5b59118f3450d526a3ccec9a644e0ebe31ef5
Arief Badal <1042170@hr.nl>
2022-11-13 19:44:50 +0100

    Refactor conform Pep8 convention

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+""" Main file for the entire Game
+    It is required to install pygame to run the game.
+    Only if this script is used the game will run
+ """
 from tic_tac_toe import Game
 
 if __name__ == "__main__":
