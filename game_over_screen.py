10725cf0d90ca3d8fcabdd9158e230ecd284bcfa
Arief Badal <1042170@hr.nl>
2022-11-13 19:40:54 +0100

    Refactor conform Pep8 convention

--- game_over_screen.py
+++ game_over_screen.py
@@ -1,18 +1,22 @@
+""" 
+    Game over window which is shown after a player has won the game
+    It is required to install pygame to run the game.
+    Contains the following methods:
+        * draw_text() - draws the text if a player won the game
+ """
 import pygame
 
+
 class GameOverScreen():
+    """ A class to create a GameOverScreen object
+         if a player has won the game"""
     def __init__(self):
-        self.font = pygame.font.SysFont('Arial', 20)
-        # self.screen = screen
-    
-    def draw_text(self, player, window, x, y):
-        game_over_text = f"Player {player} has won the game"
-        text = self.font.render(game_over_text, True, (0,0,0))
-        window.blit(text,(x, y))
+        self.font = pygame.font.SysFont('Arial', 30)
+
+    def draw_text(self, player, window, x_pos, y_pos):
+        """ Draws the text for the winning player """
+        game_over_text = f"Player {player} has won the game!"
+        text = self.font.render(game_over_text, True, (0, 0, 0))
+        window.blit(text, (x_pos, y_pos))
         pygame.display.update()
         return text
-
-    # def draw_rectangle(self, window, color):
-    #     rect = pygame.Rect((200,200), (20,20))
-    #     return pygame.draw.rect(window, color, rect)
-        
