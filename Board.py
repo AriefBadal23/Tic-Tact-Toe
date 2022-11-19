c55e0fb18f6746bff4aa768237a77b5d161e2e7b
Arief Badal <1042170@hr.nl>
2022-11-19 20:17:18 +0100

    Refactor code and add private methods and atrributes

--- Board.py
+++ Board.py
@@ -16,111 +16,156 @@ import numpy
 import pygame
 
 
-class Board():
-    """ A class to create the tic-tac-toe play board """
+class Board:
+    """A class to create the tic-tac-toe play board"""
+
     def __init__(self):
         self.rows = 3
         self.columns = 3
-        self.board = numpy.zeros((self.rows, self.columns))
+        self.__board = numpy.zeros((self.rows, self.columns))
         self.color = (255, 255, 255)
         self.square_width = 200
         self.line_width = 20
         self.width = 600
         self.line_color = (255, 255, 255)
+        
+    
+    def get_board(self):
+        return self.__board
 
     def draw_shape(self, col, row, player):
-        """ Creates the tic tac toe player board with rows and columns 3x3 """
+        """Creates the tic tac toe player board with rows and columns 3x3"""
         # Starts with zero so, 0,1,2
-        self.board[col][row] = player
+        game_board = self.get_board()
+        game_board[col][row] = player
+        # self.__board[col][row] = player
         # print(self.board)
 
-    def check_available_square(self, col, row):
-        """ Checks if the square is already marked """
-        if self.board[col][row] == 0:
+    def __check_available_square(self, col, row):
+        """Checks if the square is already marked"""
+        if self.__board[col][row] == 0:
             return True
-        elif self.board[col][row] == 1 or self.board[col][row] == 2:
+        elif self.__board[col][row] == 1 or self.__board[col][row] == 2:
             return False
 
     def draw_circle(self, window, color, x, y):
-        """ Draws a circle on the surface """
-        circle = pygame.draw.circle(window, color, (x, y),50, 10)
+        """Draws a circle on the surface"""
+        circle = pygame.draw.circle(window, color, (x, y), 50, 10)
         pygame.display.update()
         return circle
 
     def draw_lines(self, window):
-        """ Draw the horizontal and vertical lines for the tic tac toe game """
-        pygame.draw.line(window, self.color, (self.square_width, 0),
-                         (self.square_width, 600), self.line_width)
-        pygame.draw.line(window, self.color, (self.square_width*2, 0),
-                         (self.square_width*2, 600), self.line_width)
+        """Draw the horizontal and vertical lines for the tic tac toe game"""
+        pygame.draw.line(
+            window,
+            self.color,
+            (self.square_width, 0),
+            (self.square_width, 600),
+            self.line_width,
+        )
+        pygame.draw.line(
+            window,
+            self.color,
+            (self.square_width * 2, 0),
+            (self.square_width * 2, 600),
+            self.line_width,
+        )
 
         # color, start-end pos
         # x,y, x, y
-        pygame.draw.line(window, self.color, (0, self.square_width),
-                         (self.width, self.square_width), self.line_width)
+        pygame.draw.line(
+            window,
+            self.color,
+            (0, self.square_width),
+            (self.width, self.square_width),
+            self.line_width,
+        )
 
-        pygame.draw.line(window, self.color, (0, self.square_width * 2),
-                         (self.width, self.square_width * 2), self.line_width)
+        pygame.draw.line(
+            window,
+            self.color,
+            (0, self.square_width * 2),
+            (self.width, self.square_width * 2),
+            self.line_width,
+        )
         pygame.display.update()
 
-    def draw_horizontal_winning_line(self, window, col):
-        """ Draws hortizontal winning line """
+    def __draw_horizontal_winning_line(self, window, col):
+        """Draws hortizontal winning line"""
         pos_y = col * 200 + 100
-        pygame.draw.line(window, (255, 0,0), (15, pos_y),
-                        (self.width - 15, pos_y), 15)
+        pygame.draw.line(window, (255, 0, 0), (15, pos_y), (self.width - 15, pos_y), 15)
         pygame.display.update()
         return True
 
-    def draw_vertical_winning_line(self, window, row):
-        """ Draws the vertical winning line """
+    def __draw_vertical_winning_line(self, window, row):
+        """Draws the vertical winning line"""
         pos_x = row * 200 + 100
-        pygame.draw.line(window, (self.line_color), (pos_x, 25), (pos_x, self.width - 15), 15)
+        pygame.draw.line(
+            window, (self.line_color), (pos_x, 25), (pos_x, self.width - 15), 15
+        )
         pygame.display.update()
         return True
 
-    def draw_left_diagonal_winning_line(self, window, row):
-        """ Draw the left diagnal winning line """
+    def __draw_left_diagonal_winning_line(self, window, row):
+        """Draw the left diagnal winning line"""
         pos_x = row * 200 + 100
-        pygame.draw.line(window, (self.line_color), (90, 25), (pos_x, self.width - 15), 15)
+        pygame.draw.line(
+            window, (self.line_color), (90, 25), (pos_x, self.width - 15), 15
+        )
         pygame.display.update()
         return True
 
-    def draw_diagonal_winning_line_2(self, window):
-        """ Draw the right diagnal winning line """
-        pygame.draw.line(window, (self.line_color), (self.width -100, 90), (100, self.width), 15)
+    def __draw_diagonal_winning_line_2(self, window):
+        """Draw the right diagnal winning line"""
+        pygame.draw.line(
+            window, (self.line_color), (self.width - 100, 90), (100, self.width), 15
+        )
         pygame.display.update()
         return True
 
-
-
-    def check_win(self, player, window):
-        """ Method which check of the player has won """
+    def __check_win(self, player, window):
+        """Method which checks if the player has won"""
         for col in range(self.columns):
-            if self.board[col][0] == player and self.board[col][1] == player and self.board[col][2] == player:
-                self.draw_horizontal_winning_line(window, col)
+            if (
+                self.__board[col][0] == player
+                and self.__board[col][1] == player
+                and self.__board[col][2] == player
+            ):
+                self.__draw_horizontal_winning_line(window, col)
                 return True
 
-
         for row in range(self.rows):
-            if self.board[0][row] == player and self.board[1][row] == player and self.board[2][row] == player:
-                self.draw_vertical_winning_line(window, row)
+            if (
+                self.__board[0][row] == player
+                and self.__board[1][row] == player
+                and self.__board[2][row] == player
+            ):
+                self.__draw_vertical_winning_line(window, row)
                 return True
 
         # check diagnal win
-        if self.board[0][0] == player and self.board[1][1] ==player and self.board[2][2] == player:
-            self.draw_left_diagonal_winning_line(window, row)
+        if (
+            self.__board[0][0] == player
+            and self.__board[1][1] == player
+            and self.__board[2][2] == player
+        ):
+            self.__draw_left_diagonal_winning_line(window, row)
             return True
-        
-        elif self.board[2][0] == player and self.board[1][1] == player and self.board[0][2] == player:
-            self.draw_diagonal_winning_line_2(window)
+
+        elif (
+            self.__board[2][0] == player
+            and self.__board[1][1] == player
+            and self.__board[0][2] == player
+        ):
+            self.__draw_diagonal_winning_line_2(window)
             return True
-  
-    def mark_board_full(self, row, col):
+
+    def __mark_board_full(self, row, col):
         marked_spots = 0
         for row in range(self.rows):
             for col in range(self.columns):
-                if self.board[col][row] == 1 or self.board[col][row] == 2 :
-                    marked_spots +=1
+                if self.__board[col][row] == 1 or self.__board[col][row] == 2:
+                    marked_spots += 1
                     # print(marked_spots)
                     if marked_spots == 9:
                         print("All spotts have been marked!")
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -26,13 +26,14 @@ class Game:
     run(self)
         runs the tic-tac-toe game
     """
+
     def __init__(self):
         self.running = True
         self.player = 1
         pygame.init()
 
     def run(self):
-        """ Runs the tic-tac-toe game """
+        """Runs the tic-tac-toe game"""
         window = Window(600, 600)
         main_window = window.get_window()
         game_over_screen = GameOverScreen()
@@ -51,34 +52,45 @@ class Game:
                     mouse_y = int(event.pos[0] // 200)
                     mouse_x = int(event.pos[1] // 200)
 
-                    if board.check_available_square(mouse_x, mouse_y) is True:
+                    if board._Board__check_available_square(mouse_x, mouse_y) is True:
                         if self.player == 1:
                             board.draw_shape(mouse_x, mouse_y, self.player)
-                            if board.mark_board_full(mouse_x, mouse_y):
+                            if board._Board__mark_board_full(mouse_x, mouse_y):
                                 self.running = False
                             mouse_position = pygame.mouse.get_pos()
                             x_pos, y_pos = mouse_position
-                            if board.draw_circle(main_window, (0, 255, 0), x_pos, y_pos):
-                                if board.check_win(self.player, main_window) is True:
+                            if board.draw_circle(
+                                main_window, (0, 255, 0), x_pos, y_pos
+                            ):
+                                if (
+                                    board._Board__check_win(self.player, main_window)
+                                    is True
+                                ):
                                     pygame.display.update()
-                                    game_over_screen.draw_text(self.player,
-                                                               main_window, 150 , 300)
-                                    time.sleep(5)
-                                    self.running = False
+                                    game_over_screen.show_popup(
+                                        self.player, main_window, 150, 300
+                                    )
+                                    # time.sleep(5)
+                                    # self.running = False
                             self.player = 2
 
                         elif self.player == 2:
                             # mouse_x and mouse_y = uses the 3x3 raster
                             # and x_pos and y_post uses
                             # the real mouse position
-                             
+
                             mouse_position = pygame.mouse.get_pos()
                             x_pos, y_pos = mouse_position
                             board.draw_shape(mouse_x, mouse_y, self.player)
                             board.draw_circle(main_window, (255, 0, 0), x_pos, y_pos)
-                            if board.check_win(self.player, main_window) is True:
+                            if (
+                                board._Board__check_win(self.player, main_window)
+                                is True
+                            ):
                                 pygame.display.update()
-                                game_over_screen.draw_text(self.player, main_window, 200 , 300)
-                                time.sleep(5)                        
-                                self.running = False
-                            self.player = 1                            
+                                game_over_screen.show_popup(
+                                    self.player, main_window, 200, 300
+                                )
+                                # time.sleep(5)
+                                # self.running = False
+                            self.player = 1
