100bb097197b9048c9827fe939ffbd9f42a619c9
Arief Badal <1042170@hr.nl>
2022-12-31 20:57:32 +0100

    Add functionality in main file

--- run.py
+++ run.py
@@ -9,7 +9,16 @@ if __name__ == "__main__":
     tic_tact_toe_network = Network()
     app = Game()
     tic_tact_toe_network.connect_to_server()
+    # Gets the connected players
     player = tic_tact_toe_network.get_players()
-    app.set_player(player)
-   
+
+    # Add the player to playerslist
+    joined_player = app.set_player(player)
+
+    # Gets the joined player with the connection that has been made
+    app.get_player(joined_player)
+
+    # # Retrieve the players positions
+    positions = app.player_positions
+    app.get_positions(joined_player,positions)   
     app.run()
