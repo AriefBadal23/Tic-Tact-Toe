43355aad86cfce56d37e17c69355b41470d88382
Arief Badal <1042170@hr.nl>
2023-01-11 13:57:39 +0100

    Solve merge conflicts

--- Server.py
+++ Server.py
@@ -9,7 +9,7 @@ class Server():
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.MAX_PLAYERS = 5
         self.connections = []
-        self.amount_of_connections_list = []
+        self.players = []
         self.players_has_joined = []
 
 
@@ -26,10 +26,15 @@ class Server():
     def threaded_client(self, conn):
         """ When an session of the game is created start a new thread """
         while True:
-            amount_of_connections = 0
-            pass
-            # create new code to handle received data ONLY
-            
+            data = conn.recv(1024)
+            if data:
+                # print(pickle.loads(data))
+                print((f'Received: {str(pickle.loads(data))}'))
+                self.broadcast(pickle.dumps(data))
+            else:
+                self.connections.remove(conn)
+                self.server_socket.close()
+
 
     def listen_to_connections(self):
         self.server_socket.bind((self.IP_ADDRESS, self.PORT))
@@ -41,12 +46,33 @@ class Server():
             # Accept any new conenctions that has been made
             client_socket, client_address = self.server_socket.accept()
             print(f'{client_address} has connected')
+            start_new_thread(self.receive_data, (client_socket,))
+            amount_of_connections += 1
+            self.players.append(amount_of_connections)
+            print(f'{amount_of_connections} connections has been made')
+            client_socket.send(str(self.players).encode())
+
+            if len(self.players) == 1:
+                player_1_joined = {
+                    "player": self.players[0],
+                    "CAN_PLAY": True
+                }
+                self.players_has_joined.append(player_1_joined)
 
 
+            elif len(self.players) == 2:
+                player_2_joined = {
+                    "player": self.players[1],
+                    "CAN_PLAY": False
+                }
+                self.players_has_joined.append(player_2_joined)
             
 
             start_new_thread(self.threaded_client, (client_socket,))
 
+            print(f'Joined: {self.players_has_joined}')
+            client_socket.send(pickle.dumps(self.players_has_joined))
+            
 
 game_server = Server()
 game_server.listen_to_connections()
--- game.py
+++ game.py
@@ -5,9 +5,9 @@ it contains the follwoing methods:
 """
 import pygame
 
-from board import Board
+from Board import Board
 from game_over_screen import GameOverScreen
-from window import Window
+from Window import Window
 import pickle
 import socket
 import threading
@@ -25,10 +25,21 @@ class Network():
     def connect_to_server(self):
         self.client_socket.connect((self.IP_ADDRESS, self.PORT_NUMBER))
 
-    # create new methods for the client side how to handle new sended data
+    def send_pos(self, x_pos, y_pos):
+            message = pickle.dumps([x_pos, y_pos])
+            print(pickle.loads(message))
+            # print(str(message))
 
-
-
+            self.client_socket.send(message)
+            print(f'sent: {message}')
+            return message
+    
+    def parse_player_pos(self):
+        # received_pos is in positions ipv bytes!
+        received_pos = pickle.loads(self.client_socket.recv(1024))
+        print(f'Parsed player pos now:{received_pos}')
+        return received_pos
+    
 
 
 class Game():
@@ -51,8 +62,26 @@ class Game():
     def __init__(self):
         self.__running = True
         self.game_network = Network()
+        self.is_player_1_turn = False
+        self.is_player_2_turn = False
+        self.player_2 = 2
         pygame.init()
     
+    def get_pos(self, obj):
+        Game.positions.append(obj)
+        # print(Game.positions)
+        return Game.positions
+
+    def set_players(self):
+        get_players = self.game_network.client_socket.recv(1024)
+        return get_players
+    
+    def get_updated_pos(self, pos):
+        pass
+
+
+
+
     def run(self):
         """Runs the tic-tac-toe game"""
         window = Window(600, 600)
@@ -61,6 +90,26 @@ class Game():
         board = Board()
         board.draw_lines(main_window)
         self.game_network.connect_to_server()
+        # start_new_thread
+        self.game_network.client_socket.recv(1024)
+        new_playsers = self.set_players()
+
+        if new_playsers:
+            players = pickle.loads(new_playsers)
+            joined_players = []
+            joined_players.append(players)
+            # available_players.append(recv_player)
+            for player in joined_players:
+                print(f'Received player:{player[0]}')
+                if player[0]["player"] == 1 and player[0]["CAN_PLAY"] == True:
+                    self.__player = 1
+                    self.is_player_1_turn = True
+
+                else:
+                    self.player_2 = 2
+        
+                     
+
 
         while self.__running:
             for event in pygame.event.get():
@@ -69,10 +118,52 @@ class Game():
                     pygame.quit()
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                   pass
-                # rewrite the logic for the entire game
-                   
+                    mouse_y = int(event.pos[0] // 200)
+                    mouse_x = int(event.pos[1] // 200)
+    
+                    if board._Board__check_available_square(mouse_x, mouse_y) is True:
+                        if self.__player == 1 and self.is_player_1_turn:
+                            board.draw_game_board(mouse_x, mouse_y, self.__player)
+                            if board._Board__mark_board_full(mouse_x, mouse_y):
+                                self.__running = False
+
+                            mouse_position = pygame.mouse.get_pos()
+                            x_pos, y_pos = mouse_position
 
 
+                            if board.draw_circle(main_window, (0, 255, 0), x_pos, y_pos):
+
+
+                                if (board._Board__check_win(self.__player, main_window) is True):
+                                    pygame.display.update()
+                                    game_over_screen.show_popup(self.__player, main_window, 150, 300)
+                                    # time.sleep(5)
+                                    # self.running = False
+                            print(f'Value: {self.player_2}')
+                            self.is_player_1_turn = False
+                            self.is_player_2_turn = True
+                            self.game_network.send_pos(x_pos, y_pos)
+
+
+                        elif self.player_2 == 2 and self.is_player_2_turn == True:
+                            # mouse_x and mouse_y = uses the 3x3 raster
+                            # and x_pos and y_post uses
+                            # the real mouse position
+                            mouse_position = pygame.mouse.get_pos()
+                            x_pos, y_pos = mouse_position
+
+                            board.draw_circle(main_window, (255, 0, 0), x_pos, y_pos)
+                            if (board._Board__check_win(self.__player, main_window) is True):
+                                pygame.display.update()
+                                game_over_screen.show_popup(self.__player, main_window, 200, 300)
+                                # time.sleep(5)
+                                # self.running = False
+                            self.is_player_2_turn = False
+                            self.is_player_1_turn = True
+                            self.__player = 1
+                            self.game_network.send_pos(x_pos, y_pos)
+                        start_new_thread(self.game_network.parse_player_pos, ())
+
+                        # self.game_network.parse_player_pos()
 
                         
