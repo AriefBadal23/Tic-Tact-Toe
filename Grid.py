eccdb7ecbd76f887118251fef6fe368a4c10b415
Arief Badal <1042170@hr.nl>
2023-01-11 19:10:11 +0100

    Add updated version of game logic and grid for the board

--- Grid.py
+++ Grid.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy
 
 class Grid():
     def __init__(self):
@@ -6,7 +7,29 @@ class Grid():
                             ((0, 400), (600, 400)),
                             ((200, 0), (200, 600)), 
                             ((400, 0), (400, 600))]
+        self.board = [[0 for x in range(3)] for y in range(3)]
     
+    def print_grid(self):
+        """ Printing the grid of the game (only for debugging) """
+        for pos in self.board:
+            print(pos)
+
+    def get_cell_value(self, pos_y, pos_x):
+        """ Getter method to retrieve the row and column of the value """
+        return self.board[pos_y][pos_x]
+    
+    def set_cell_value(self, pos_y, pos_x, value):
+        self.board[pos_x][pos_y] = value
+
+
     def draw(self, surface):
+        "Draw method to draw the grid of the board"
         for line in self.grid_lines:
             pygame.draw.line(surface, (200, 200, 200), line[0], line[1], 2)
+
+    def get_mouse(self, x, y, player):
+        """ Retreive the mouse positions with the right player """
+        if player == "X":
+            self.set_cell_value(x, y, "X")
+        elif player == "O":
+            self.set_cell_value(x, y, "O")
--- game.py
+++ game.py
@@ -3,91 +3,41 @@ It is required to install pygame to run the game.
 it contains the follwoing methods:
     * run() - runs the game
 """
-import time
 
 import pygame
 
-from Board import Board
-from game_over_screen import GameOverScreen
 from Window import Window
+from Grid import Grid
 
 
-class Game:
-    """A class to create tic-tac-toe game
-    Attributes
-    -----------
-    running: bool
-    player: int
-    Methods
-    -----------
-    run(self)
-        runs the tic-tac-toe game
-    """
+def run():
+    """Runs the tic-tac-toe game"""
+    window = Window(600, 600)
+    main_window = window.get_window()
+    grid = Grid()
+    grid.draw(main_window)
+    pygame.display.update()
+    player = "X"
 
-    def __init__(self):
-        self.__running = True
-        self.__player = 1
-        pygame.init()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0] == True:
+                    pos = pygame.mouse.get_pos()
+                    grid.get_mouse(pos[0] // 200, pos[1] // 200, player)
+                    # if player is X switch to O and vice versa
+                    if player == "X":
+                        player = "O"
+                    else:
+                        player = "X"
+                    grid.print_grid()
 
-    def run(self):
-        """Runs the tic-tac-toe game"""
-        window = Window(600, 600)
-        main_window = window.get_window()
-        game_over_screen = GameOverScreen()
-        board = Board()
-        board.draw_lines(main_window)
 
-        while self.__running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.__running = False
-                    pygame.quit()
 
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    # The window has an height and width of 600 px;
-                    #  each square to mark is 200 px
-                    mouse_y = int(event.pos[0] // 200)
-                    mouse_x = int(event.pos[1] // 200)
 
-                    if board._Board__check_available_square(mouse_x, mouse_y) is True:
-                        if self.__player == 1:
-                            board.draw_shape(mouse_x, mouse_y, self.__player)
-                            if board._Board__mark_board_full(mouse_x, mouse_y):
-                                self.__running = False
-                            mouse_position = pygame.mouse.get_pos()
-                            x_pos, y_pos = mouse_position
-                            if board.draw_circle(
-                                main_window, (0, 255, 0), x_pos, y_pos
-                            ):
-                                if (
-                                    board._Board__check_win(self.__player, main_window)
-                                    is True
-                                ):
-                                    pygame.display.update()
-                                    game_over_screen.show_popup(
-                                        self.__player, main_window, 150, 300
-                                    )
-                                    # time.sleep(5)
-                                    # self.running = False
-                            self.__player = 2
-
-                        elif self.__player == 2:
-                            # mouse_x and mouse_y = uses the 3x3 raster
-                            # and x_pos and y_post uses
-                            # the real mouse position
-
-                            mouse_position = pygame.mouse.get_pos()
-                            x_pos, y_pos = mouse_position
-                            board.draw_shape(mouse_x, mouse_y, self.__player)
-                            board.draw_circle(main_window, (255, 0, 0), x_pos, y_pos)
-                            if (
-                                board._Board__check_win(self.__player, main_window)
-                                is True
-                            ):
-                                pygame.display.update()
-                                game_over_screen.show_popup(
-                                    self.__player, main_window, 200, 300
-                                )
-                                # time.sleep(5)
-                                # self.running = False
-                            self.__player = 1
+if __name__ == "__main__":
+    run()
